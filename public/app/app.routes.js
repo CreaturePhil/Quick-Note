c274eb61a95b8ac985568f38e795766063022d82
Phil <birkal@outlook.com>
2015-01-27 21:47:55 -0800

    Add signup/login pages

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -15,6 +15,12 @@
       .when('/about', {
         templateUrl: 'app/views/about.html'
       })
+      .when('/signup', {
+        templateUrl: 'app/views/signup.html'
+      })
+      .when('/login', {
+        templateUrl: 'app/views/login.html'
+      })
       .when('/404', {
         templateUrl: 'app/views/404.html'
       })
